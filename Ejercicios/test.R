4dbc7ce3373b8061df8a7b78c378c020f5c9c612
Jennifer <jennifer.velozo@usach.cl>
2021-12-09 01:27:16 -0300

    Resumen de lecturas

--- Ejercicios/test.R
+++ Ejercicios/test.R
@@ -1,4 +1,5 @@
 library ( dplyr )
+library(pwr)
 
 # Lectura del archivo 
 dir <- "E:"
@@ -9,35 +10,62 @@ set.seed(127) # semilla inicial
 
 no_anemia <- datos %>% filter(anaemia == 0)
 no_anemia <- no_anemia[["creatinine_phosphokinase"]]
+
 si_anemia <- datos %>% filter(anaemia == 1)
 si_anemia <- si_anemia[["creatinine_phosphokinase"]]
+
 muestra_no_anemia<-sample(no_anemia,25)
 muestra_si_anemia<-sample(si_anemia,25)
 
-# Verifiación de condiciones para usa t de Student
+
+# Verifiación de condiciones para usar t de Student
 normalidad_no <- shapiro.test (muestra_no_anemia)
 print (normalidad_no)
 normalidad_si <- shapiro.test (muestra_si_anemia)
 print (normalidad_si)
 
+# Se obtienen las medias de cada muestra 
+media_si_anemia <- mean(muestra_si_anemia)
+media_no_anemia <- mean(muestra_no_anemia)
+
+# Se obtiene la desviación estándar de cada muestra
+sd_si_anemia <- sd(muestra_si_anemia)
+sd_no_anemia <- sd(muestra_no_anemia)
+
 alfa <- 0.05
+n= 25
 
-prueba <- t.test ( x = muestra_no_anemia,
-                  y = muestra_si_anemia,
-                   paired = FALSE ,
-                    alternative = "two.sided",
-                    mu = 0 ,
-                    conf.level = 1 - alfa )
+# Se realiza la prueba t de Student para muestras independientes
+prueba <- t.test(x = muestra_no_anemia,
+                 y = muestra_si_anemia, 
+                 paired = FALSE,
+                 alternative = "two.sided",
+                 mu = 0,
+                 conf.level = 1- alfa)
 
-print ( prueba )
+print(prueba)
 
-desv_no <- sd(muestra_no_anemia)
-desv_si <- sd(muestra_si_anemia)
-diferencia <- mean(muestra_no_anemia) - mean(muestra_si_anemia)
+# Se calcula la desviación estándar agrupada
+sp <- sqrt((sd_si_anemia^2*(n - 1)+sd_no_anemia^2*(n - 1)) / (n+n-2))
 
-poder <- power.t.test(n=c(25,25), delta=diferencia, sd=c(desv_no,desv_si), sig.level = alfa, power=NULL, 
-                      type = "two.sample", alternative = "two.sided")
+# Se calcula la d de cohen
+factor_correccion <- (n+n-3)/(n+n-2.25)
+d <- ((media_si_anemia-media_no_anemia)/sp)*factor_correccion
+
+# Se calcula el poder
+poder <- pwr.t.test ( n =n,
+                          d =d,
+                          sig.level = alfa,
+                          power = NULL,
+                          type = "two.sample",
+                          alternative = "two.sided")
 print(poder)
+
+
+
+
+
+
 
 
 
